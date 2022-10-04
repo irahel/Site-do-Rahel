c0aebe706d0d93b889b17ec8c01924c2fc0be553
Isaac Rahel <isaac_rahel@live.com>
2022-10-04 15:43:16 -0300

    fix: scroll animation

--- src/components/Scroll.tsx
+++ src/components/Scroll.tsx
@@ -1,21 +1,10 @@
+import UseAnimations from "react-useanimations";
+import arrowDown from "react-useanimations/lib/arrowDown";
+
 function Scroll() {
   return (
-    <div className="relative bottom-24 self-center opacity-40 h-11">
-      <span
-        className="block relative h-12 w-6
-        border-[.25rem] border-solid border-white
-        rounded-full
-        "
-      >
-        <span
-          className="
-            block absolute left-1/2 top-2
-            bg-white h-2 w-2 -ml-1
-            rounded-full
-            animate-scroll
-            "
-        ></span>
-      </span>
+    <div className="relative bottom-24 self-center h-11 items-center">
+      <UseAnimations animation={arrowDown} size={56} strokeColor="#F7F7F7" />
     </div>
   );
 }
