cebc78acb09fae381d402a8a8228540cc66f1d69
Isaac Rahel <isaac_rahel@live.com>
2022-10-17 20:28:12 -0300

    fix: fix avatar resize

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -44,7 +44,7 @@ function Home() {
           </h3>
         </article>
         <img
-          src="/avatar.png?nf_resize=fit&w=420"
+          src="https://github.com/irahel/Site-do-Rahel/blob/main/public/img/avatar.png?raw=true?nf_resize=fit&w=420"
           loading="lazy"
           alt="Rahel's avatar"
           className="h-[420px] w-[420px] rounded-full bg-blue"
